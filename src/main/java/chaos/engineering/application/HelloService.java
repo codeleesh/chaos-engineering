9a58f09eb98bae07c00efda7cf3c867d237a3deb
sangho39.lee <sangho39.lee@samsung.com>
2022-02-11 16:54:14 +0900

    feat hello DTO 추가

--- src/main/java/chaos/engineering/application/HelloService.java
+++ src/main/java/chaos/engineering/application/HelloService.java
@@ -1,10 +1,19 @@
 package chaos.engineering.application;
 
+import chaos.engineering.domain.Hello;
+import chaos.engineering.domain.HelloRepository;
+import chaos.engineering.dto.HelloRequest;
+import chaos.engineering.dto.HelloResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class HelloService {
-    public String hello() {
-        return "Hello";
+    private final HelloRepository repository;
+
+    public HelloResponse hello(final HelloRequest request) {
+        final Hello saveHello = repository.save(request.toEntity());
+        return HelloResponse.from(saveHello);
     }
 }
--- src/main/java/chaos/engineering/controller/HelloController.java
+++ src/main/java/chaos/engineering/controller/HelloController.java
@@ -1,8 +1,12 @@
 package chaos.engineering.controller;
 
 import chaos.engineering.application.HelloService;
+import chaos.engineering.dto.HelloRequest;
+import chaos.engineering.dto.HelloResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,8 +15,9 @@ public class HelloController {
 
     private final HelloService service;
 
-    @GetMapping("index")
-    public String hello() {
-        return service.hello();
+    @PostMapping("hello")
+    public ResponseEntity<HelloResponse> hello(@RequestBody final HelloRequest request) {
+        final HelloResponse hello = service.hello(request);
+        return ResponseEntity.ok(hello);
     }
 }
